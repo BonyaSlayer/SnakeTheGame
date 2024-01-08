183235b53e9880976e1a80c80b347bce1302193c
BonyaSlayer <zlobasterus@gmail.com>
2024-01-08 14:25:28 +1000

    add functional

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace Snake
 {
@@ -7,29 +8,35 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            //Point p1 = new Point(1, 3, '*');                   
-
-            //Point p2 = new Point(4, 5, '#');
-
-            //p1.Draw();
-            //p2.Draw();
+            
             Console.SetWindowSize(80, 25);
             Console.SetBufferSize(80, 25);
 
+            //Отрисовка рамки
             HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
             HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
             VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
             VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-
             upLine.Draw();
             downLine.Draw();
             leftLine.Draw();
             rightLine.Draw();
 
+            //Отрисовка точек
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
-
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300); snake.Move();
+            Thread.Sleep(300); snake.Move();
+            Thread.Sleep(300); snake.Move();
+            Thread.Sleep(300); snake.Move();
+            Thread.Sleep(300); snake.Move();
+            Thread.Sleep(300); snake.Move();
+            Thread.Sleep(300); snake.Move();
+            Thread.Sleep(300); snake.Move();
 
             Console.ReadKey();
         }
--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -8,17 +8,40 @@ namespace Snake
 {
     internal class Snake : Figure
     {
+        Direction direction;
 
-        public Snake(Point tail, int lenght, Direction direction)
+
+        public Snake(Point tail, int lenght, Direction _direction)
         {
             pList = new List<Point>();
+
             for (int i = 0; i < lenght; i++)
-            {
+            {                
+                direction = _direction;
                 Point p = new Point(tail);
                 p.Move(i, direction);
                 pList.Add(p);
             }
         }
 
+        internal void Move()
+        {
+            Point tail = pList.First();
+            pList.Remove(tail);
+            Point head = GetNextPoint();
+            pList.Add(head);
+
+            tail.Clear();
+            head.Draw();
+
+        }
+
+        public Point GetNextPoint() 
+        {
+            Point head = pList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, direction);
+            return nextPoint;
+        }
     }
 }

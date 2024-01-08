79c92ecbdd8298abfff3e8433baa1a3a0ad2560b
BonyaSlayer <zlobasterus@gmail.com>
2024-01-08 16:59:06 +1000

    add Walls add functional

--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -78,5 +78,16 @@ namespace Snake
                     return true;
             return false;
         }
+
+        internal bool IsHitTail()
+        {
+            var head = pList.Last();
+            for (int i = 0; i < pList.Count - 2; i++)
+            {
+                if (head.IsHit(pList[i]))
+                    return true;
+            }
+            return false;
+        }
     }
 }
